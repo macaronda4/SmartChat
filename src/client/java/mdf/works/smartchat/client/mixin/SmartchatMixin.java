730fc05acaffcbfa7859e1da2b2fab6331680b26
macaron <macaronda4@gmail.com>
2026-07-06 13:35:59 +0900

    Change Mapping

--- src/client/java/mdf/works/smartchat/client/mixin/SmartchatMixin.java
+++ src/client/java/mdf/works/smartchat/client/mixin/SmartchatMixin.java
@@ -1,9 +1,11 @@
 package mdf.works.smartchat.client.mixin;
 
 import mdf.works.smartchat.client.ReceveChatCallBack;
-import mdf.works.smartchat.client.SmartchatClient;
 import net.minecraft.client.gui.components.ChatComponent;
-import net.minecraft.client.multiplayer.chat.GuiMessageSource;import net.minecraft.client.multiplayer.chat.GuiMessageTag;import net.minecraft.network.chat.Component;import net.minecraft.network.chat.MessageSignature;import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.GuiMessageTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MessageSignature;
+import org.spongepowered.asm.mixin.Mixin;
 
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,10 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatComponent.class)
 public class SmartchatMixin {
-
-
     @Inject(at = @At("TAIL"), method = "addMessage")
-    private void test(Component contents, MessageSignature signature, GuiMessageSource source, GuiMessageTag tag, CallbackInfo ci) {
+    private void ReceveChatEvent(Component contents, MessageSignature signature, GuiMessageTag tag, CallbackInfo ci) {
         ReceveChatCallBack.EVENT.invoker().interact(contents.getString());
     }
 }
